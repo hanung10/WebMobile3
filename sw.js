b9df00ba558f9a0c9aa67a72562af461e3eaa4b9
hanung10 <146154785+hanung10@users.noreply.github.com>
2025-06-11 14:36:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
-// Event saat service worker diinstal pertama kali
+
 self.addEventListener("install", async event => {
   const cache = await caches.open("pwa-assets");
-  // Simpan file statis ke cache untuk digunakan offline
+  // it stores all resources on first SW install
   cache.addAll([
     "/", 
     "app.js", 
     "index.html",
-    "pendaftaran.html",
     "style.css", 
-    "img/kampus.png"
-  ]);
+    "pendaftaran.html"
+    "img/kampus.jpg"
+  ]); 
+});
+ 
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request).then(cachedResponse => {
+	   // It can update the cache to serve updated content on the next request
+         return cachedResponse || fetch(event.request);
+     }
+   )
+  )
 });
 
-// Event saat browser mencoba fetch resource (online/offline)
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      // Jika ditemukan di cache, tampilkan dari cache. Jika tidak, ambil dari jaringan.
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
