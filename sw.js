856567f8288b3bb3b567f3c2d4cc13c97c58f3ac
hanung10 <146154785+hanung10@users.noreply.github.com>
2025-06-11 14:26:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,13 +4,11 @@ self.addEventListener("install", async event => {
   // Simpan file statis ke cache untuk digunakan offline
   cache.addAll([
     "/", 
+    "app.js", 
     "index.html",
     "pendaftaran.html",
-    "app.js", 
     "style.css", 
-    "img/kampus.png", 
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    "img/kampus.png"
   ]);
 });
 
